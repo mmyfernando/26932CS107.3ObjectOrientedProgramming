130e50ef021fa87836e29df13ba3b31f0066e052
mmyfernando <136342762+mmyfernando@users.noreply.github.com>
2023-07-31 12:46:50 +0530

    Add files via upload

---
+++ Lab 1 Q 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1_Q_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My Batch is 22.2");
+            Console.WriteLine("My Name is Manith");
+            Console.ReadKey();
+
+        }
+    }
+}
